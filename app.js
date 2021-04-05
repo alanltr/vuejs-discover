e27b6b2db61dc8a0376a36b3df4b3b8ceebc4f0f
alanltr <letrocqueralan@gmail.com>
2021-04-05 19:35:38 +0200

    implementation d'une methode lors du clic sur un btn

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ var app = new Vue({
   el: '#app',
   data: {
     user: '',
-    products: ['Pizza', 'Cheese', 'Tacos', 'Hamburger']
+    products: ['Pizza', 'Cheese', 'Tacos', 'Hamburger'],
+    commandes: []
+  },
+  methods: {
+    commander: () => {
+      console.log('ok')
+    }
   }
 })
